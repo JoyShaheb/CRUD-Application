e1cc5c9c971ee2eda9c113e3ebec911b9fbde017
Joy Shaheb <istiaque.joy@northsouth.edu>
2022-01-23 17:06:17 +0600

    updated

---
+++ crud basics/main.js
@@ -0,0 +1,46 @@
+let form = document.getElementById("form");
+let input = document.getElementById("input");
+let msg = document.getElementById("msg");
+let posts = document.getElementById("posts");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("button clicked");
+  formValidation();
+});
+
+let formValidation = () => {
+  if (input.value === "") {
+    msg.innerHTML = "Post cannot be blank";
+    console.log("failure");
+  } else {
+    console.log("successs");
+    msg.innerHTML = "";
+    acceptData();
+  }
+};
+
+let data = {};
+
+let acceptData = () => {
+  data["text"] = input.value;
+  console.log(data);
+  createPost();
+};
+
+let createPost = () => {
+  posts.innerHTML += `
+  <div>
+    <p>${data.text}</p>
+    <span class="options">
+      <i class="fas fa-edit"></i>
+      <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
+    </span>
+  </div>
+  `;
+  input.value = "";
+};
+
+let deletePost = (e) => {
+  e.parentElement.parentElement.remove();
+};
